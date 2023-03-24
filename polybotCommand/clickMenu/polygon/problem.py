e1444330504aba8f6457df1aeb4f8bd81b23ebfe
Crystal <Crystal>
2023-03-23 21:51:56 -0600

    Command: problem info

--- polybotCommand/clickMenu/polygon/problem.py
+++ polybotCommand/clickMenu/polygon/problem.py
@@ -1,6 +1,9 @@
 import sys
 import click
+import awsUtils.dynamoContestsDB as contestsDB
+import awsUtils.dynamoContestRequestsDB as contestRequestDB
 import polygon.polygonApi as PolygonApi
+from tabulate import tabulate
 
 
 @click.group()
@@ -25,9 +28,29 @@ async def create(contestid):
 @problem.command()
 @click.argument("contestid")
 @click.argument("problemid")
-async def see_info(contestid, problemid):
-    res = await PolygonApi.info(problemId=problemid)
-    click.echo(res)
+@click.pass_context
+async def info(ctx, contestid, problemid):
+    mine = is_mine(ctx.obj["groupId"], contestid)
+    if mine != 2:
+        click.echo("Polybot doesn't have access to this problem")
+        return
+    contest = await PolygonApi.contest(contestid)
+    if contest == None:
+        click.echo("Contest is missing write access.")
+        return
+    data = []
+    for c in contest:
+        if c.letter == problemid:
+            data.append(["name", c.name])
+            data.append(["owner", c.owner])
+            info = await PolygonApi.info(c.id)
+            if info != None:
+                data.append(["timeLimit", str(info.timeLimit) + "ms"])
+                data.append(["memoryLimit", str(info.memoryLimit) + "mb"])
+                data.append(["interactive", info.interactive])
+            break
+    table = "```\n" + tabulate(data, tablefmt="pretty") + "\n```"
+    click.echo(table)
 
 
 @problem.command()
@@ -52,3 +75,12 @@ async def see_files(contestid, problemid):
 @click.argument("packageid")
 async def see_package(contestid, problemid, packageid):
     pass
+
+
+def is_mine(groupId, contestId):
+    res = contestsDB.query(contestId, "polygon")
+    if len(res):
+        if res[0]["groupId"] == groupId:
+            return 2  # mine
+        return 1  # owned
+    return 0  # not owned
