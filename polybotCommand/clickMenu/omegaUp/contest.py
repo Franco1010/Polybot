a0f92157ca5b24e1a9b9d99192eb1e7b91e318cc
Ivan Borquez <i.borquez@cignuz.com>
2023-03-24 03:27:59 -0600

    Rename the omegaUp wrapper to omegaUpWrapper

--- polybotCommand/clickMenu/omegaUp/contest.py
+++ polybotCommand/clickMenu/omegaUp/contest.py
@@ -22,28 +22,55 @@ async def help(ctx):
     """Show this message and exit."""
     click.echo(contest.get_help(ctx))
 
+
 @contest.command()
 @click.pass_context
 @click.argument("contest_title", type=str)
 @click.argument("contest_start", type=str)
 @click.argument("contest_duration", type=int)
-@click.option("--custom_id",  type=str, help="Custom id for the contest")
+@click.option("--custom_id", type=str, help="Custom id for the contest")
 @click.option("--description", type=str, help="Description of the contest")
-@click.option("--languages", type=str, help="Languages available in the contest 'c11-clang'|'c11-gcc'|'cat'|'cpp11-clang'|'cpp11-gcc'|'cpp17-clang'|'cpp17-gcc'|'cpp20-clang'|'cpp20-gcc'|'cs'|'go'|'hs'|'java'|'js'|'kj'|'kp'|'kt'|'lua'|'pas'|'py2'|'py3'|'rb'|'rs'|null")
-@click.option("--scoreboard", type=int, help="Integer representing the porcentage of time the scoreboard will be visible")
-async def create(ctx, contest_title, contest_start, contest_duration, custom_id, description, languages, scoreboard):
-    """Create contest test.\n 
+@click.option(
+    "--languages",
+    type=str,
+    help="Languages available in the contest 'c11-clang'|'c11-gcc'|'cat'|'cpp11-clang'|'cpp11-gcc'|'cpp17-clang'|'cpp17-gcc'|'cpp20-clang'|'cpp20-gcc'|'cs'|'go'|'hs'|'java'|'js'|'kj'|'kp'|'kt'|'lua'|'pas'|'py2'|'py3'|'rb'|'rs'|null",
+)
+@click.option(
+    "--scoreboard",
+    type=int,
+    help="Integer representing the porcentage of time the scoreboard will be visible",
+)
+async def create(
+    ctx,
+    contest_title,
+    contest_start,
+    contest_duration,
+    custom_id,
+    description,
+    languages,
+    scoreboard,
+):
+    """Create contest test.\n
     contest_title: Title of the contest.\n
     contest_start: Start date of the contest in the ISO date format, YYYY-MM-DDTHH:mm.\n
     contest_duration: Integer of the duration in minutes.\n
     """
     contest_id = str(uuid.uuid4())[:22]
-    if(custom_id):
-        contest_id = custom_id  
-    creation = OmegaUpApi.create_contest(contest_id, contest_title, contest_start, contest_duration, description, languages, scoreboard)
+    if custom_id:
+        contest_id = custom_id
+    creation = OmegaUpApi.create_contest(
+        contest_id,
+        contest_title,
+        contest_start,
+        contest_duration,
+        description,
+        languages,
+        scoreboard,
+    )
     click.echo(creation)
     if creation == "Contest created":
         contestsDB.create_item(contest_id, "omegaUp", contest_title, ctx.obj["groupId"])
+
 
 @contest.command(name="list")
 @click.pass_context
--- src_layer/omegaUp/omegaUpApi.py
+++ src_layer/omegaUp/omegaUpApi.py
@@ -15,25 +15,53 @@ username = account["username"]
 password = account["password"]
 client_class = omegaup.api.Client(username=username, password=password)
 
-def create_contest(contest_id, contest_title, contest_start, contest_duration, contest_description, contest_languages, contest_scoreboard):
+
+def create_contest(
+    contest_id,
+    contest_title,
+    contest_start,
+    contest_duration,
+    contest_description,
+    contest_languages,
+    contest_scoreboard,
+):
     contest_class = omegaup.api.Contest(client=client_class)
-    start_time=datetime.fromisoformat(contest_start)
-    end_time=start_time + timedelta(minutes=contest_duration)
+    start_time = datetime.fromisoformat(contest_start)
+    end_time = start_time + timedelta(minutes=contest_duration)
     unix_start_time = start_time.timestamp()
     unix_end_time = end_time.timestamp()
     alias = contest_id
     title = contest_title
-    if(contest_description):
+    if contest_description:
         description = contest_description
     else:
-        description = "Contest named " + title + " with a duration of " + str(contest_duration) + " minutes."
-    if(contest_languages):
+        description = (
+            "Contest named "
+            + title
+            + " with a duration of "
+            + str(contest_duration)
+            + " minutes."
+        )
+    if contest_languages:
         languages = contest_languages
     else:
         languages = DEFAULT.LANGUAGES
-    if(contest_scoreboard):
+    if contest_scoreboard:
         scoreboard = contest_scoreboard
     else:
         scoreboard = DEFAULT.SCOREBOARD
-    contest_class.create(alias=alias, title=title, description=description, start_time=unix_start_time, finish_time=unix_end_time, scoreboard=scoreboard, points_decay_factor = DEFAULT.POINTS_DECAY_FACTOR, submissions_gap = DEFAULT.SUBMISSION_GAP, penalty=DEFAULT.PENALTY, feedback=DEFAULT.FEEDBACK, penalty_type=DEFAULT.PENALTY_TYPE, languages=languages)
+    contest_class.create(
+        alias=alias,
+        title=title,
+        description=description,
+        start_time=unix_start_time,
+        finish_time=unix_end_time,
+        scoreboard=scoreboard,
+        points_decay_factor=DEFAULT.POINTS_DECAY_FACTOR,
+        submissions_gap=DEFAULT.SUBMISSION_GAP,
+        penalty=DEFAULT.PENALTY,
+        feedback=DEFAULT.FEEDBACK,
+        penalty_type=DEFAULT.PENALTY_TYPE,
+        languages=languages,
+    )
     return "Contest created"
